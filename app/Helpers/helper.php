9584252021075ed8cc53585f7334503353d4ac6d
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-01-06 00:24:21 -0500

    feat: add helper
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

---
+++ app/Helpers/helper.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * VPN - Server-side software for centralized administration and node management of a VPN service.
+ * Copyright (C) 2025 Elvis Yerel Roman Concha
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+if (!function_exists('location')) {
+    function location(string $ip)
+    {
+        $url = "http://ip-api.com/json/{$ip}?fields=status,country,countryCode,regionName,city,query";
+
+        $response = Http::get($url);
+
+        if ($response->successful() && $response['status'] === 'success') {
+            return [
+                'ip' => $response['query'],
+                'country' => $response['country'],
+                'country_code' => $response['countryCode'],
+                'region' => $response['regionName'],
+                'city' => $response['city'],
+            ];
+        }
+
+        return null;
+    }
+}
