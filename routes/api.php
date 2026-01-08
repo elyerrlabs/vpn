7ea2b0a19d5d6c5267c90f0cecd3e305b88bb99b
Elvis Yerel Roman <yerel9212@yahoo.es>
2026-01-08 17:26:19 -0500

    feat: added new routes
    Signed-off-by: Elvis Yerel Roman <yerel9212@yahoo.es>

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,8 @@ Route::group([
     'as' => 'admin.'
 ], function () {
 
-    Route::resource('servers', \Vpn\App\Http\Controllers\Api\Admin\ServerController::class);
-    Route::resource('wireguard', \Vpn\App\Http\Controllers\Api\Admin\WireguardController::class);
+    Route::resource('servers', \Vpn\App\Http\Controllers\Api\Admin\ServerController::class)->except('create', 'edit');
+    Route::resource('wireguard', \Vpn\App\Http\Controllers\Api\Admin\WireguardController::class)->except('create', 'edit');
 });
 
 
@@ -21,7 +21,9 @@ Route::group([
     'as' => 'users.'
 ], function () {
 
-    Route::resource('servers', \Vpn\App\Http\Controllers\Api\User\ServerController::class)->only('index');
+    Route::get('/servers/all', [\Vpn\App\Http\Controllers\Api\User\ServerController::class, 'listServers'])->name('lists.servers');
+    Route::resource('servers', \Vpn\App\Http\Controllers\Api\User\ServerController::class)->except('edit', 'create');
+
     Route::resource('wireguard', \Vpn\App\Http\Controllers\Api\User\WireguardController::class)->only('index');
     Route::resource('peers', \Vpn\App\Http\Controllers\Api\User\PeerController::class)->only('index', 'store', 'update', 'destroy');
 });
